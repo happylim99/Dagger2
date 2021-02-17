6c8a00c6a2e57312edf3e85814a8e880df3d4176
sean <happylim99@gmail.com>
2021-02-17 23:19:38 +0800

    prove component level singleton does not work in activity

--- app/src/main/java/com/sean/dagger2/MainActivity.kt
+++ app/src/main/java/com/sean/dagger2/MainActivity.kt
@@ -23,8 +23,15 @@ class MainActivity : AppCompatActivity() {
             .build()
         carComponent.inject(this)
 
+        var carComponent1 = DaggerCarComponent.builder()
+            .powerCapacity(111)
+            .engineCapacity(222)
+            .build()
+        carComponent1.inject(this)
 
+        car = carComponent.getCar()
         car.start()
+        car1 = carComponent1.getCar()
         car1.start()
     }
 }
